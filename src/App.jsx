6bc56f2082d8d8fd0e096766371ff3c355b6a3e2
Nanda kishore <nandakishore14n@gmail.com>
2025-02-01 22:06:00 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,40 @@ import { Routes, Route, Link } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Events from "./pages/Events";
 import About from "./pages/About";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './App.css'; // Import custom CSS for additional styling
 
 function Home() {
-  return <h1>About Us</h1>;
+  return (
+    <div className="home">
+      <h1>Welcome to Our Website</h1>
+      <p>Discover more about us and our events.</p>
+    </div>
+  );
 }
-
-
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> <br />
-      <Link to="/about">About</Link> <br />
-      <Link to="/events">Events</Link> <br />
-      <Link to="/contact">ContactUs</Link> <br />
-      <p>Mrisa</p>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/events">Events</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
